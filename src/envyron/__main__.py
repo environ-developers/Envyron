834dbbe96082376d21994b74d516d46d10741c0e
Edan Bainglass <edan.bainglass@gmail.com>
2021-12-02 07:20:31 -0600

    Added testing paths
    - Comand-line executable main() function
    - General purpose Jupyter notebook

---
+++ src/envyron/__main__.py
@@ -0,0 +1,3 @@
+from .cui.main import main
+
+main()
---
+++ src/envyron/cui/main.py
@@ -0,0 +1,8 @@
+def run_job() -> None:
+    """Execute the current job."""
+    pass
+
+
+def main():
+    """Execute Envyron from command line."""
+    run_job()
