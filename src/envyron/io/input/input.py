589452e1f73e84b537eb32d1992399bfbb384b6e
Edan Bainglass <edan.bainglass@gmail.com>
2022-02-28 11:04:28 -0600

    Made input file reading optional

--- src/envyron/io/input/input.py
+++ src/envyron/io/input/input.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Optional
 import ruamel.yaml as yaml
 
 from .base import InputModel
@@ -10,15 +11,36 @@ class Input:
     Class for reading, casting, and validating Environ's input parameters.
     """
 
-    def __init__(self, natoms: int, filename: str = 'environ.yml') -> None:
+    def __init__(self, natoms: int, filename: Optional[str] = None) -> None:
+
+        self.param_dict = {
+            'control': {},
+            'environment': {},
+            'ions': {},
+            'system': {},
+            'electrolyte': {},
+            'semiconductor': {},
+            'solvent': {},
+            'electrostatics': {},
+            'pbc': {},
+        }
+
+        if filename is not None:
+            self.read(filename)
+
+        self.params = InputModel(natoms, **self.param_dict)
+
+        del self.param_dict
+
+    def read(self, filename: str) -> None:
+        """Read and validate input file."""
 
         # read input file
-        self.file = Path(filename).absolute()        
+        self.file = Path(filename).absolute()
         with open(self.file) as f:
-            param_dict: dict = yaml.safe_load(f)
+            input_dict = yaml.safe_load(f)
 
-        # validate and set input parameters
-        self.params = InputModel(natoms, **param_dict)
+        self.param_dict.update(input_dict)
 
 
 def main():
@@ -38,12 +60,12 @@ def main():
         metavar='filename',
         help='Input file name',
         type=str,
-        default='test.yml',
+        default=None,
     )
 
     natoms, filename = vars(parser.parse_args()).values()
 
-    my_input = Input(natoms=natoms, filename=filename).params
+    my_input = Input(natoms, filename=filename).params
 
     for section in my_input:
         name, fields = section
--- stubs/envyron/io/input/input.pyi
+++ stubs/envyron/io/input/input.pyi
@@ -1,9 +1,11 @@
 from .base import InputModel as InputModel
-from typing import Any
+from typing import Any, Optional
 
 class Input:
-    file: Any
+    param_dict: Any
     params: Any
-    def __init__(self, natoms: int, filename: str = ...) -> None: ...
+    def __init__(self, natoms: int, filename: Optional[str] = ...) -> None: ...
+    file: Any
+    def read(self, filename: str) -> None: ...
 
 def main() -> None: ...
