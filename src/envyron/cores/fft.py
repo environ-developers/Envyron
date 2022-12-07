85117c32696a00d7d45b66ba97e580d60a3300f8
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2022-12-06 17:36:20 -0700

    test implementation of derivatives with fft

--- src/envyron/cores/fft.py
+++ src/envyron/cores/fft.py
@@ -1,33 +1,58 @@
 from numpy import ndarray
 
+import numpy as np
+
 from envyron.cores.core import NumericalCore
 
+from ..domains import EnvironGrid
 from ..representations import EnvironDensity, EnvironGradient, EnvironHessian
 from ..representations.functions import FunctionContainer
 
-
+from dftpy.field import ReciprocalField
 class FFTCore(NumericalCore):
     """docstring"""
 
+    def __init__(self, grid: EnvironGrid) -> None:
+        super().__init__(grid)
+        self.reciprocal_grid = grid.get_reciprocal()
+
     def gradient(self, density: EnvironDensity) -> EnvironGradient:
         """docstring"""
-        return EnvironGradient(density.grid, density.gradient(), 'gradient')
+        density_g = density.fft()
+        imag = 0 + 1j
+        gradient_g = self.reciprocal_grid.g * density_g * imag
+        gradient_g = ReciprocalField(grid=self.reciprocal_grid, rank=3, griddata_3d=gradient_g)
+        gradient = gradient_g.ifft(force_real=True)
+        return EnvironGradient(self.grid, gradient, 'gradient')
+#        return EnvironGradient(density.grid, density.gradient(), 'gradient')
 
     def divergence(self, gradient: EnvironGradient) -> EnvironDensity:
         """docstring"""
-        raise NotImplementedError()
+        gradient_g = gradient.fft()
+        imag = 0 + 1j
+        divergence_g = np.einsum('l...,l...',self.reciprocal_grid.g,gradient_g) * imag
+        divergence_g = ReciprocalField(grid=self.reciprocal_grid, griddata_3d=divergence_g)
+        divergence = divergence_g.ifft(force_real=True)
+        return EnvironDensity(self.grid, divergence, 'divergence')
 
     def laplacian(self, density: EnvironDensity) -> EnvironDensity:
         """docstring"""
-        return EnvironDensity(density.grid, density.laplacian(), 'laplacian')
+        density_g = density.fft()
+        laplacian_g = -self.reciprocal_grid.gg * density_g
+        laplacian_g = ReciprocalField(grid=self.reciprocal_grid, griddata_3d=laplacian_g)
+        laplacian = laplacian_g.ifft(force_real=True)
+        return EnvironDensity(self.grid, laplacian, 'laplacian')
 
-    def hessian(
-        self,
-        density: EnvironDensity,
-        gradient: EnvironGradient,
-    ) -> EnvironGradient:
+    def hessian(self, density: EnvironDensity) -> EnvironHessian:
         """docstring"""
-        raise NotImplementedError()
+        density_g = density.fft()
+        hessian_g = -np.einsum('i,j',self.reciprocal_grid.g,self.reciprocal_grid.g) * density_g
+        hessian_g = hessian_g.reshape(9, *self.grid.nr)
+        hessian = EnvironHessian(self.grid, 'hessian')
+        for ipol in np.arange(9):
+            aux_g = ReciprocalField(grid=self.reciprocal_grid, griddata_3d=hessian_g[ipol,:,:,:])
+            hessian[ipol,:,:,:] = aux_g.ifft(force_real=True)
+        return hessian
 
     def convolution_density(
         self,
