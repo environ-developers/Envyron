823139da64c764183882a5f18a42fd896b080373
Edan Bainglass <edan.bainglass@gmail.com>
2022-12-08 15:12:02 -0700

    Implemented `ElectrostaticSolverSetup` class

--- src/envyron/solvers/setup.py
+++ src/envyron/solvers/setup.py
@@ -1,4 +1,54 @@
-class SolverSetup:
+import numpy as np
+
+from ..solvers import ElectrostaticSolver
+from ..representations import EnvironDensity
+from ..physical import EnvironCharges
+
+
+class ElectrostaticSolverSetup:
     """
     Setup parameters of an electrostatic solver.
     """
+
+    def __init__(
+        self,
+        problem: str,
+        solver: ElectrostaticSolver,
+        inner: ElectrostaticSolver,
+    ) -> None:
+        self.problem = problem
+        self.solver = solver
+        self.inner = inner
+
+    def solve(self, charges: EnvironCharges) -> EnvironDensity:
+        """docstring"""
+
+        if self.problem == 'poisson':
+            return self.solver.poisson(charges)
+        elif self.problem == 'generalized':
+            if not charges.dielectric:
+                raise ValueError("missing dielectric")
+            return self.solver.generelized(charges)
+        elif self.problem in ('linpb', 'linmodpb'):
+            if not charges.electrolyte:
+                raise ValueError("missing electrolyte")
+            return self.solver.linearized_pb(charges)
+        elif self.problem in ('pb', 'modpb'):
+            if not charges.electrolyte:
+                raise ValueError("missing electrolyte")
+            if not self.inner:
+                if not charges.dielectric:
+                    raise ValueError("missing dielectric")
+                return self.solver.pb_nested(charges, inner=self.inner)
+            else:
+                return self.solver.pb_nested(charges)
+        else:
+            raise ValueError(f'Unsupported problem: {self.problem}')
+
+    def compute_energy(self) -> float:
+        """docstring"""
+        raise NotImplementedError()
+
+    def compute_force(self) -> np.ndarray:
+        """docstring"""
+        raise NotImplementedError()
