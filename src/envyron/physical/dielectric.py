63b29f4214c7042d16f1732bb5c12c486441a6ce
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2022-12-08 15:17:15 -0700

    Added dielectric attributes and update method

--- src/envyron/physical/dielectric.py
+++ src/envyron/physical/dielectric.py
@@ -1,6 +1,8 @@
 from ..representations import EnvironDensity
 from ..boundaries import EnvironBoundary
 
+import numpy as np
+
 
 class EnvironDielectric:
     """docstring"""
@@ -15,7 +17,11 @@ class EnvironDielectric:
     ) -> None:
         self.boundary = boundary
         self.constant = constant
-        
+
+        self.epsilon = EnvironDensity(grid=boundary.grid)
+        self.depsilon = EnvironDensity(grid=boundary.grid)
+        self.gradlogepsilon = EnvironDensity(grid=boundary.grid, rank=3)
+
         self.need_gradient = need_gradient
         if self.need_gradient:
             self.gradient = EnvironDensity(grid=boundary.grid, rank=3)
@@ -28,16 +34,26 @@ class EnvironDielectric:
         if self.need_auxiliary:
             self.iterative = EnvironDensity(grid=boundary.grid)
 
-        self.epsilon = EnvironDensity(grid=boundary.grid)
-        self.depsilon = EnvironDensity(grid=boundary.grid)
-        self.gradlogepsilon = EnvironDensity(grid=boundary.grid, rank=3)
-
-        # polarization density
+        # polarization density and its multipoles
         self.density = EnvironDensity(grid=boundary.grid)
+        self.charge = 0.
+        self.dipole = np.zeros(3)
+        self.quadrupole = np.zeros(3)
+
+        self.updating = False
 
     def update(self) -> None:
         """docstring"""
-        raise NotImplementedError
+        # if the boundary is updating flag the dielectric as updating
+        if self.boundary.update_status > 0:
+            self.updating = True
+
+        # if the dielectric needs to be updated
+        if self.updating:
+            # only do it if the boundary is ready (update_status = 2)
+            if self.boundary.update_status == 2:
+                self.of_boundary()
+                self.updating = False
 
     def of_boundary(self) -> None:
         """docstring"""
