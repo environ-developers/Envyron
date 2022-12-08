51fd6c89cd19d0fe7000b2056293639c00684e41
Eric Read <ericread2013@gmail.com>
2022-12-08 16:40:37 -0700

    Changed access to preconditioner helper methods to protected

--- src/envyron/solvers/gradient.py
+++ src/envyron/solvers/gradient.py
@@ -27,27 +27,6 @@ class GradientSolver(IterativeSolver):
         self.steepest_descent = steepest_descent
         self.verbosity = verbosity
 
-    def __preconditioner_sqrt__(
-        self,
-        rk: EnvironDensity,
-        inv_sqrt_epsilon: Optional[Union[EnvironDensity, None]] = None
-    ) -> EnvironDensity:
-        """docstring"""
-        if inv_sqrt_epsilon is None:
-            inv_sqrt_epsilon = np.reciprocal(np.sqrt(self.dielectric.epsilon))
-        return self.cores.electrostatics.poisson(
-            rk * inv_sqrt_epsilon) * inv_sqrt_epsilon
-
-    def __preconditioner_left__(
-        self,
-        rk: EnvironDensity,
-        inv_epsilon: Optional[Union[EnvironDensity, None]] = None
-    ) -> EnvironDensity:
-        """docstring"""
-        if inv_epsilon is None:
-            inv_epsilon = np.reciprocal(self.dielectric.epsilon)
-        return self.cores.electrostatics.poisson(rk * inv_epsilon)
-
     @overload
     @dispatch(EnvironCharges)
     def generalized(self, charges: EnvironCharges) -> EnvironDensity:
@@ -128,3 +107,24 @@ class GradientSolver(IterativeSolver):
             rzold = rznew
 
         return phi
+
+    def _preconditioner_sqrt(
+        self,
+        rk: EnvironDensity,
+        inv_sqrt_epsilon: Optional[Union[EnvironDensity, None]] = None
+    ) -> EnvironDensity:
+        """docstring"""
+        if inv_sqrt_epsilon is None:
+            inv_sqrt_epsilon = np.reciprocal(np.sqrt(self.dielectric.epsilon))
+        return self.cores.electrostatics.poisson(
+            rk * inv_sqrt_epsilon) * inv_sqrt_epsilon
+
+    def _preconditioner_left(
+        self,
+        rk: EnvironDensity,
+        inv_epsilon: Optional[Union[EnvironDensity, None]] = None
+    ) -> EnvironDensity:
+        """docstring"""
+        if inv_epsilon is None:
+            inv_epsilon = np.reciprocal(self.dielectric.epsilon)
+        return self.cores.electrostatics.poisson(rk * inv_epsilon)
