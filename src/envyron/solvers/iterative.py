5e85eb73b9d49af68050e080e2041a8bf2706b91
Edan Bainglass <edan.bainglass@gmail.com>
2022-12-08 15:17:11 -0700

    Implemented `IterativeSolver` abstract class

--- src/envyron/solvers/iterative.py
+++ src/envyron/solvers/iterative.py
@@ -1,7 +1,24 @@
 from abc import ABC
 
-from .solver import Solver
+from ..solvers import ElectrostaticSolver, DirectSolver
+from ..cores import CoreContainer
 
 
-class IterativeSolver(Solver, ABC):
-    """docstring"""
+class IterativeSolver(ElectrostaticSolver, ABC):
+    """
+    docstring
+    """
+
+    def __init__(
+        self,
+        cores: CoreContainer,
+        direct: DirectSolver,
+        maxiter: int,
+        tol: float,
+        auxiliary: str = '',
+    ) -> None:
+        super().__init__(cores)
+        self.direct = direct
+        self.maxiter = maxiter
+        self.tol = tol
+        self.auxiliary = auxiliary
