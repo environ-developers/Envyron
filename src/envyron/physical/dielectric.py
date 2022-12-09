ec7a6960df413635755957255cce45855b658537
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2022-12-08 17:21:26 -0700

    Implemented all methods of EnvironDielectric class

--- src/envyron/physical/dielectric.py
+++ src/envyron/physical/dielectric.py
@@ -1,5 +1,6 @@
 from ..representations import EnvironDensity
-from ..boundaries import EnvironBoundary
+from ..boundaries import EnvironBoundary, ElectronicBoundary
+from ..utils.constants import FPI
 
 import numpy as np
 
@@ -44,6 +45,7 @@ class EnvironDielectric:
 
     def update(self) -> None:
         """docstring"""
+
         # if the boundary is updating flag the dielectric as updating
         if self.boundary.update_status > 0:
             self.updating = True
@@ -57,16 +59,70 @@ class EnvironDielectric:
 
     def of_boundary(self) -> None:
         """docstring"""
-        raise NotImplementedError
 
-    def of_potential(self) -> None:
-        """docstring"""
-        raise NotImplementedError
+        # for the time being we just consider a uniform background (no regions)
+        background = EnvironDensity(grid=self.boundary.grid)
+        background[:, :, :] = self.constant
 
-    def de_dboundary(self) -> None:
-        """docstring"""
-        raise NotImplementedError
+        if isinstance(self.boundary, ElectronicBoundary):
+            self.epsilon[:] = np.exp(
+                np.log(self.background) * (1. - self.boundary.switch))
+            self.depsilon[:] = -self.epsilon * np.log(self.background)
+            dlogeps = -np.log(self.background)
+            if self.need_factsqrt:
+                d2eps = self.epsilon * np.log(self.background)**2
+        else:
+            self.epsilon[:] = 1. + (self.background - 1.) * \
+                (1. - self.boundary.switch)
+            self.depsilon[:] = 1. - self.background
+            dlogeps = self.depsilon / self.epsilon
+            if self.need_factsqrt: d2eps = 0.
 
-    def dv_dboundary(self) -> None:
+        # compute derived quantities
+
+        self.gradlogepsilon[:] = self.boundary.gradient * dlogeps
+
+        if self.need_gradient:
+            self.gradient[:] = self.boundary.gradient * self.depsilon
+
+        if self.need_factsqrt:
+            self.factsqrt[:] = ( d2eps - 0.5 * self.depsilon**2 / self.epsilon ) * \
+                self.boundary.gradient.modulus**2 + \
+                self.depsilon * self.boundary.laplacian
+            self.factsqrt *= 0.5 / FPI
+
+    def of_potential(
+        self,
+        charges: EnvironDensity,
+        potential: EnvironDensity,
+    ) -> None:
         """docstring"""
-        raise NotImplementedError
+
+        gradient = self.boundary.cores.derivatives.gradient(potential)
+        self.density[:] = gradient.scalar_gradient_product(self.gradlogepsilon)
+        self.density[:] = self.density / FPI + (
+            1. - self.epsilon) / self.epsilon * charges
+        self.charge = self.density.charge
+
+    def de_dboundary(
+        self,
+        potential: EnvironDensity,
+        de_dboundary: EnvironDensity,
+    ) -> None:
+        """docstring"""
+
+        gradient = self.boundary.cores.derivatives.gradient(potential)
+        de_dboundary -= gradient.modulus**2 * self.depsilon * 0.5 / FPI
+
+    def dv_dboundary(
+        self,
+        potential: EnvironDensity,
+        dpotential: EnvironDensity,
+        dv_dboundary: EnvironDensity,
+    ) -> None:
+        """docstring"""
+
+        gradient = self.boundary.cores.derivatives.gradient(potential)
+        dgradient = self.boundary.cores.derivatives.gradient(dpotential)
+        dv_dboundary -= gradient.scalar_gradient_product(
+            dgradient) * self.depsilon / FPI
