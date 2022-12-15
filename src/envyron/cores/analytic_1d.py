2b377d5de771e14c4348ccc47d26674d526956df
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2022-12-14 22:42:45 -0700

    Some more work on `Analytic1DCore`

--- src/envyron/cores/analytic_1d.py
+++ src/envyron/cores/analytic_1d.py
@@ -10,29 +10,32 @@ class Analytic1DCore(NumericalCore):
 
     def __init__(self, grid: EnvironGrid, dim: int, axis: int) -> None:
 
-        if dim == 3 or dim < 0 :
-            raise ValueError("Wrong dimensions for analytic one dimensional core")
+        super().__init__(grid)
+
+        if dim == 3 or dim < 0:
+            raise ValueError(
+                "Wrong dimensions for analytic one dimensional core")
         self.dim = dim
         self.pdim = 3 - dim
 
         if (dim == 1 or dim == 2) and (axis > 3 or axis < 1):
-            raise ValueError("Wrong choice of axis for analytic one dimensional core")
+            raise ValueError(
+                "Wrong choice of axis for analytic one dimensional core")
         self.axis = axis
 
-        self.x = np.zeros((self.pdim,self.grid.nnr))
+        self.r = np.zeros(grid.r.shape)
 
         self.origin = np.zeros(3)
 
-        super().__init__(grid)
-
-        if dim == 0 :
+        if dim == 0:
             self.size = grid.volume
-        elif dim == 1 :
+        elif dim == 1:
             self.size = grid.volume / grid.cell.diagonal()[axis]
-        elif dim == 2 :
+        elif dim == 2:
             self.size = grid.cell.diagonal()[axis]
 
-    def update_origin(self,origin):
+    def update_origin(self, origin):
         """docstring"""
 
         self.origin = origin
+        self.r, _ = self.grid.get_min_distance(self.origin,self.dim,self.axis)
