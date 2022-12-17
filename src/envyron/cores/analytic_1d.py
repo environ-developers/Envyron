12501039594e9910a7ba91a2d9042c2857284c94
Edan Bainglass <edan.bainglass@gmail.com>
2022-12-17 09:52:35 -0600

    Formatting

--- src/envyron/cores/analytic_1d.py
+++ src/envyron/cores/analytic_1d.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from ..domains import EnvironGrid
-
 from .core import NumericalCore
 
 
@@ -9,22 +8,21 @@ class Analytic1DCore(NumericalCore):
     """docstring"""
 
     def __init__(self, grid: EnvironGrid, dim: int, axis: int) -> None:
-
         super().__init__(grid)
 
         if dim == 3 or dim < 0:
             raise ValueError(
                 "Wrong dimensions for analytic one dimensional core")
+
         self.dim = dim
         self.pdim = 3 - dim
 
         if (dim == 1 or dim == 2) and (axis > 3 or axis < 1):
             raise ValueError(
                 "Wrong choice of axis for analytic one dimensional core")
+
         self.axis = axis
-
         self.r = np.zeros(grid.r.shape)
-
         self.origin = np.zeros(3)
 
         if dim == 0:
@@ -38,4 +36,9 @@ class Analytic1DCore(NumericalCore):
         """docstring"""
 
         self.origin = origin
-        self.r, _ = self.grid.get_min_distance(self.origin,self.dim,self.axis)
+
+        self.r, _ = self.grid.get_min_distance(
+            self.origin,
+            self.dim,
+            self.axis,
+        )
