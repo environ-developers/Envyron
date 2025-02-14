459b28e65caa80a070acbc3c6518f89781945cc3
Jakob Filser <jakobfilser@boisestate.edu>
2025-02-14 15:01:00 -0700

    implement missing function based on Fortran version
    TODO: Untested so far!

--- src/envyron/boundaries/ionic.py
+++ src/envyron/boundaries/ionic.py
@@ -251,4 +251,16 @@ class IonicBoundary(EnvironBoundary):
 
     def _update_soft_spheres(self) -> None:
         """docstring"""
-        pass
+
+        for i in range(self.ions.count):
+            soft_sphere = self.soft_spheres.functions[i]
+            solvationrad = self.ions.iontypes[self.ions.itypes[i]]
+
+            # field-aware scaling of soft-sphere radii
+            if self.field_aware:
+                raise NotImplementedError
+            else:
+                field_scale = 1.
+
+            self.soft_spheres.functions[i].pos = self.ions.coords[:,i]
+            self.soft_spheres.functions[i].width = solvationrad * self.alpha * field_scale
